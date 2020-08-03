6086533cefb94af921d40fa13c54931769db4bb8
crysmart <425331631@qq.com>
2020-08-03 11:00:08 +0800

    create vue template project

---
+++ hello-vue/src/views/demo/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        THIS IS TEST DEMO
+        <el-input v-model="data" placeholder="ceshiu"></el-input>
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            data:"fsdfsf"
+        }
+    },
+    methods: {
+        
+    }
+    
+}
+</script>
+<style scoped>
+div{
+    color: red;
+}
+</style>
